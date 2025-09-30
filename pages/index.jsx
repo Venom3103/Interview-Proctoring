ad18184c6d462c46faec32be38829afba7ba1aa7
Venom3103 <ayushkumar3300@gmail.com>
2025-09-30 22:53:24 +0530

    Changes made

--- pages/index.jsx
+++ pages/index.jsx
@@ -19,14 +19,14 @@ export default function Home(){
   return (
     <div className="min-h-screen flex items-center justify-center p-6">
       <div className="w-full max-w-md p-8 rounded-2xl card">
-        <h1 className="text-2xl font-bold mb-4 text-violet-800">Sign in to Proctoring</h1>
-        <label className="text-sm small">Role</label>
+        <h1 className="text-2xl font-bold mb-4 text-white">Sign in to Interview Proctoring</h1>
+        <label className="text-sm small text-white">Who you are ?</label>
         <div className="flex gap-2 my-3">
           <button onClick={()=>setRole('user')} className={role==='user'?'px-4 py-2 bg-indigo-600 rounded':'px-4 py-2 bg-gray-700 rounded'}>Candidate</button>
           <button onClick={()=>setRole('admin')} className={role==='admin'?'px-4 py-2 bg-indigo-600 rounded':'px-4 py-2 bg-gray-700 rounded'}>Admin</button>
         </div>
-        <input className="w-full p-3 rounded mb-3 text-black" placeholder="Your email" value={email} onChange={e=>setEmail(e.target.value)} />
-        <button onClick={sendOtp} className="w-full py-2 rounded bg-accent">Send OTP</button>
+        <input className="w-full p-3 rounded mb-3 text-black" placeholder="Type your email" value={email} onChange={e=>setEmail(e.target.value)} />
+        <button onClick={sendOtp} className="w-full py-2 rounded bg-accent text-white">Send OTP</button>
         {msg && <div className="mt-3 small">{msg}</div>}
       </div>
     </div>
